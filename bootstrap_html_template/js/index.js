345bbc30efe737c48d4429b1df4201691470539a
Ben <benjamin.a.spencer@gmail.com>
2015-11-09 18:43:35 -0500

    registration and login working on the front end

---
+++ bootstrap_html_template/js/index.js
@@ -0,0 +1,88 @@
+'use strict';
+var callback;
+var mwbapi = {
+  mwb: 'http://localhost:3000',
+
+  ajax: function(config, cb) {
+    $.ajax(config).done(function(data, textStatus, jqxhr) {
+      cb(null, data);
+    }).fail(function(jqxhr, status, error) {
+      cb({jqxher: jqxhr, status: status, error: error});
+    });
+  },
+
+  register: function register(credentials, callback) {
+    this.ajax({
+      method: 'POST',
+      url: this.mwb + '/register',
+      contentType: 'application/json',
+      data: JSON.stringify(credentials),
+      dataType: 'json'
+    }, callback);
+  },
+
+  login: function login(credentials, callback) {
+    this.ajax({
+      method: 'POST',
+      url: this.mwb + '/login',
+      contentType: 'application/json',
+      data: JSON.stringify(credentials),
+      dataType: 'json'
+    }, callback);
+  }
+};
+
+$(document).ready(function() {
+  var form2object = function(form) {
+    // debugger;
+    var data = {};
+    $(form).find('input').each(function(index, element) {
+      // debugger;
+      var type = $(this).attr('type');
+      if ($(this).attr('name') && type !== 'submit' && type !== 'hidden') {
+        data[$(this).attr('name')] = $(this).val();
+      }
+    });
+    return data;
+  };
+
+  var wrap = function wrap(root, formData) {
+    // debugger;
+    var wrapper = {};
+    wrapper[root] = formData;
+    return wrapper;
+  };
+
+  callback = function callback(error, data) {
+    if (error) {
+      console.error(error);
+      $('#result').val('status: ' + error.status + ', error: ' +error.error);
+      // return;
+    }
+    $('#result').val(JSON.stringify(data, null, 4));
+  };
+
+  $('#register').on('submit', function(e) {
+    console.log(form2object(this));
+    var credentials = wrap('credentials', form2object(this));
+    mwbapi.register(credentials, callback);
+    e.preventDefault();
+  });
+
+  $('#login').on('submit', function(e) {
+    console.error(error);
+    var credentials = wrap('credentials', form2object(this));
+    var cb = function cb(error, data) {
+      if (error) {
+        callback(error);
+        return;
+      }
+      callback(null, data);
+      $('.token').val(data.user.token);
+
+    };
+    mwbapi.login(credentials, cb);
+    console.log("You logged in!");
+  });
+
+});
