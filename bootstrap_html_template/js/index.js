878b8b4b520309651e960f2218a2e93dad97fda6
Ben <benjamin.a.spencer@gmail.com>
2015-11-12 21:27:13 -0500

    adds working update
    Note: while words do update, the server throws a Internal Server 500 Error.

--- bootstrap_html_template/js/index.js
+++ bootstrap_html_template/js/index.js
@@ -2,7 +2,7 @@
 var callback;
 var user = {
   id: null,
-  token: null
+  token: null,
 };
 
 var mwbapi = {
@@ -12,7 +12,11 @@ var mwbapi = {
     $.ajax(config).done(function(data, textStatus, jqxhr) {
       cb(null, data);
     }).fail(function(jqxhr, status, error) {
-      cb({jqxher: jqxhr, status: status, error: error});
+      cb({
+        jqxher: jqxhr,
+        status: status,
+        error: error,
+      });
     });
   },
 
@@ -22,7 +26,7 @@ var mwbapi = {
       url: this.mwb + '/register',
       contentType: 'application/json',
       data: JSON.stringify(credentials),
-      dataType: 'json'
+      dataType: 'json',
     }, callback);
   },
 
@@ -32,28 +36,28 @@ var mwbapi = {
       url: this.mwb + '/login',
       contentType: 'application/json',
       data: JSON.stringify(credentials),
-      dataType: 'json'
+      dataType: 'json',
     }, callback);
   },
 
-   //Authenticated api actions
-  logout: function (id, token, callback) {
+  //Authenticated api actions
+  logout: function(id, token, callback) {
     this.ajax({
       method: 'DELETE',
       url: this.mwb + '/logout/' + id,
       headers: {
-        Authorization: 'Token token=' + token
+        Authorization: 'Token token=' + token,
       },
-      dataType: 'json'
+      dataType: 'json',
     }, callback);
   },
 
-  createWord: function (word, token, callback) {
+  createWord: function(word, token, callback) {
     this.ajax({
       method: 'POST',
       url: this.mwb + '/words',
       headers: {
-        Authorization: 'Token token=' + token
+        Authorization: 'Token token=' + token,
       },
       contentType: 'application/json',
       data: JSON.stringify(word),
@@ -61,29 +65,29 @@ var mwbapi = {
     }, callback);
   },
 
-  searchWord: function (name, token, callback) {
+  searchWord: function(name, token, callback) {
     this.ajax({
       method: 'GET',
       url: this.mwb + '/words' + '?name=' + name,
       headers: {
-        Authorization: 'Token token=' + token
+        Authorization: 'Token token=' + token,
       },
-      dataType: 'json'
+      dataType: 'json',
     }, callback);
   },
 
-  updateWord: function (id, data, token, callback) {
+  updateWord: function(id, word, token, callback) {
     this.ajax({
       method: 'PATCH',
-      url: this.ttt + '/words/' + id,
+      url: this.mwb + '/words/' + id,
       headers: {
-        Authorization: 'Token token=' + token
+        Authorization: 'Token token=' + token,
       },
+      data: JSON.stringify(word),
       contentType: 'application/json; charset=utf-8',
-      data: JSON.stringify(data),
-      dataType: 'json'
+      dataType: 'json',
     }, callback);
-  }
+  },
 
 };
 
@@ -94,6 +98,7 @@ $(document).ready(function() {
   $('#random').hide();
   $('#favorite').hide();
   $('#logout').hide();
+  $('#updateForm').hide();
 
   var form2object = function(form) {
     var data = {};
@@ -103,6 +108,7 @@ $(document).ready(function() {
         data[$(this).attr('name')] = $(this).val();
       }
     });
+
     return data;
   };
 
@@ -115,9 +121,10 @@ $(document).ready(function() {
   callback = function callback(error, data) {
     if (error) {
       console.error(error);
-      $('#result').val('status: ' + error.status + ', error: ' +error.error);
+      $('#result').val('status: ' + error.status + ', error: ' + error.error);
       return;
     }
+
     $('#result').val(JSON.stringify(data, null, 4));
   };
 
@@ -134,15 +141,17 @@ $(document).ready(function() {
     var cb = function cb(error, data) {
       if (error) {
         callback(error);
-        $("#login").html("<strong>Error! Login fail!</strong>");
+        $('#login').html('<strong>Error! Login fail!</strong>');
         return;
       }
+
       user.token = data.user.token;
       user.id = data.user.id;
       $('.token').val(data.user.token);
       console.log(data.user.token);
 
     };
+
     mwbapi.login(credentials, cb);
     $('.login-block').hide();
     $('#search').show();
@@ -154,111 +163,99 @@ $(document).ready(function() {
 
   }); //end of login
 
-  $('#logout').click(function(e){
-      e.preventDefault();
-      var token = user.token;
-      var id = user.id;
-      mwbapi.logout(id, token, callback);
-      user.token = null;
-      $('.login-block').show();
-      $('#sidebar-wrapper').children().hide();
-      $('.container-fluid').hide();
-      $('.container-fluid').hide();
-      $('.row').hide();
-      $('.sixColumns').hide();
-      $('.u-full-width').hide();
-      $('.button-primary').hide();
-
+  $('#logout').click(function(e) {
+    e.preventDefault();
+    var token = user.token;
+    var id = user.id;
+    mwbapi.logout(id, token, callback);
+    user.token = null;
+    $('.login-block').show();
+    $('#sidebar-wrapper').children().hide();
+    $('.container-fluid').hide();
+    $('.container-fluid').hide();
+    $('.row').hide();
+    $('.sixColumns').hide();
+    $('.u-full-width').hide();
+    $('.button-primary').hide();
   }); // end of logout
 
-  $( "#define" ).click(function() {
-    $('.col-lg-12').hide();
-    $('#createNew').show();
-  });
+  $('#define').click(function() {
+      $('.col-lg-12').hide();
+      $('#createNew').show();
+    });
 
   //create a new word
   $('#createNew').on('submit', function(e) {
     var word = {
-      word : {
-      name: $('#exampleWordInput').val(),
-      definition: $('#exampleDefinition').val(),
-      sample_sentence: $('#exampleSentence').val(),
-      user_id: user.id
+      word: {
+        id: $('#exampleWordID').val(),
+        name: $('#exampleWordInput').val(),
+        definition: $('#exampleDefinition').val(),
+        sample_sentence: $('#exampleSentence').val(),
+        user_id: user.id,
       }
     };
-    var token = user.token;
-    mwbapi.createWord(word, token, function(err, data) {
+
+    mwbapi.createWord(word, user.token, function(err, data) {
       if (err) {
         console.error(err);
         return;
       }
+
       console.log(data);
     });
-    e.preventDefault();
 
-  });
+    e.preventDefault();
+  }); //end of createWord
 
   //search for a word
   $('#search').on('submit', function(e) {
-    e.preventDefault();
-    var token = user.token;
-    var name = form2object(this).word;
-    // debugger;
-    mwbapi.searchWord(name, token, function(err, data) {
+      e.preventDefault();
+      var name = form2object(this).word;
+
+      mwbapi.searchWord(name, user.token, function(err, data) {
+        if (err) {
+          console.error(err);
+          return;
+        }
+
+        $('#word').text(name);
+        $('#definition').text(data.words[0].definition);
+        $('#sentence').text(data.words[0].sample_sentence);
+        $('#wordID').text(data.words[0].id);
+      });
+    }); //end of searchWord
+
+  //updates a word
+  $('#displayWordUpdateForm').on('click', function(e) {
+      e.preventDefault();
+
+      $('.col-lg-12').hide();
+      $('#updateForm').show();
+  });
+
+
+  $('#updateForm').on('submit', function(e) {
+    var wordId = $('#updateWordID').val();
+
+    var word = {
+      word: {
+        name: $('#updateWordInput').val(),
+        definition: $('#updateDefinition').val(),
+        sample_sentence: $('#updateSentence').val(),
+        user_id: user.id
+      }
+    }
+
+    mwbapi.updateWord(wordId, word, user.token, function(err, data) {
       if (err) {
         console.error(err);
         return;
       }
+      $('.col-lg-12').show();
+      $('#createNew').hide();
       console.log(data);
-      // debugger;
-      $('#word').text(name);
-      // debugger;
-      $('#definition').text(data.words[0].definition);
-      // debugger;
-      $('#sentence').text(data.words[0].sample_sentence);
-      // debugger;
+      e.preventDefault();
     });
-
   });
-
-  // searchWord: function (name, token, callback) {
-  //   this.ajax({
-  //     method: 'GET',
-  //     url: this.mwb + '/words' + '?name=' + name,
-  //     headers: {
-  //       Authorization: 'Token token=' + token
-  //     },
-  //     dataType: 'json'
-  //   }, callback);
-  // }
-
-
-
-  //updates a word
-  $('#buttonUpdate').on('click', function(e) {
-    $('.col-lg-12').hide();
-    $('#createNew').show();
-    var updateWord = $('#word').text();
-    var updateDefinition = $('#definition').text();
-    var updateSentence = $('#sentence').text();
-    $('#exampleWordInput').text(updateWord);
-    debugger;
-  });
-// updateWord: function (id, data, token, callback) {
-//     this.ajax({
-//       method: 'PATCH',
-//       url: this.ttt + '/words/' + id,
-//       headers: {
-//         Authorization: 'Token token=' + token
-//       },
-//       contentType: 'application/json; charset=utf-8',
-//       data: JSON.stringify(data),
-//       dataType: 'json'
-//     }, callback);
-//   }
-
 });
-
-
-
-
