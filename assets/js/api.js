877b2b73f57216ae0503f3293490459fc8c4ea2d
Ben <benjamin.a.spencer@gmail.com>
2015-12-28 10:57:54 -0500

    SoC adds a api file and hide more elements on landing page

---
+++ assets/js/api.js
@@ -0,0 +1,92 @@
+'use strict';
+var callback;
+var user = {
+  id: null,
+  token: null,
+};
+
+var mwbapi = {
+  mwb: 'https://fathomless-island-1134.herokuapp.com',
+
+  ajax: function(config, cb) {
+    $.ajax(config).done(function(data, textStatus, jqxhr) {
+      cb(null, data);
+    }).fail(function(jqxhr, status, error) {
+      cb({
+        jqxher: jqxhr,
+        status: status,
+        error: error,
+      });
+    });
+  },
+
+  register: function register(credentials, callback) {
+    this.ajax({
+      method: 'POST',
+      url: this.mwb + '/register',
+      contentType: 'application/json',
+      data: JSON.stringify(credentials),
+      dataType: 'json',
+    }, callback);
+  },
+
+  login: function login(credentials, callback) {
+    this.ajax({
+      method: 'POST',
+      url: this.mwb + '/login',
+      contentType: 'application/json',
+      data: JSON.stringify(credentials),
+      dataType: 'json',
+    }, callback);
+  },
+
+  //Authenticated api actions
+  logout: function(id, token, callback) {
+    this.ajax({
+      method: 'DELETE',
+      url: this.mwb + '/logout/' + id,
+      headers: {
+        Authorization: 'Token token=' + token,
+      },
+      dataType: 'json',
+    }, callback);
+  },
+
+  createWord: function(word, token, callback) {
+    this.ajax({
+      method: 'POST',
+      url: this.mwb + '/words',
+      headers: {
+        Authorization: 'Token token=' + token,
+      },
+      contentType: 'application/json',
+      data: JSON.stringify(word),
+      dataType: 'json',
+    }, callback);
+  },
+
+  searchWord: function(name, token, callback) {
+    this.ajax({
+      method: 'GET',
+      url: this.mwb + '/words' + '?name=' + name,
+      headers: {
+        Authorization: 'Token token=' + token,
+      },
+      dataType: 'json',
+    }, callback);
+  },
+
+  updateWord: function(id, word, token, callback) {
+    this.ajax({
+      method: 'PATCH',
+      url: this.mwb + '/words/' + id,
+      headers: {
+        Authorization: 'Token token=' + token,
+      },
+      data: JSON.stringify(word),
+      contentType: 'application/json; charset=utf-8',
+      dataType: 'json',
+    }, callback);
+  },
+
+};
